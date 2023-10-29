0cde3e3f74bae4f0c41c73ab95fa44ae9c8013b4
LuisHinoS <129122338+LuisHinoS@users.noreply.github.com>
2023-10-29 15:50:43 -0300

    Create matrizTransposta.c

---
+++ matrizTransposta.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int linhas, colunas;
+
+    printf("Informe o número de linhas e colunas da matriz: ");
+    scanf("%d %d", &linhas, &colunas);
+
+    if (linhas <= 0 || colunas <= 0) {
+        printf("Número de linhas e colunas inválido.\n");
+        return 1;
+    }
+
+    int matriz[100][100], transposta[100][100];
+
+    printf("Insira os elementos da matriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (int i = 0; i < colunas; i++) {
+        for (int j = 0; j < linhas; j++) {
+            transposta[i][j] = matriz[j][i];
+        }
+    }
+
+    printf("Matriz Transposta:\n");
+    for (int i = 0; i < colunas; i++) {
+        for (int j = 0; j < linhas; j++) {
+            printf("%d ", transposta[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
